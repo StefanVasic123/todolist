f8db10b857a0421b7196006f2b5144d2bada3161
Stefan Vasic <vasicstefan123@gmail.com>
2019-11-14 20:26:11 +0100

    Updates

---
+++ precache-manifest.9ea3a547054318736ee8d11b778cbc78.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee59b79b8f9138a9cac196f594247013",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "21d82744ca60c7d5fd68",
+    "url": "/todolist/static/js/2.8a866c4b.chunk.js"
+  },
+  {
+    "revision": "f787caf360940c901ba3",
+    "url": "/todolist/static/js/main.082f17af.chunk.js"
+  },
+  {
+    "revision": "1d50dbbb921aaf55723b",
+    "url": "/todolist/static/js/runtime-main.f0d97fc7.js"
+  }
+]);
