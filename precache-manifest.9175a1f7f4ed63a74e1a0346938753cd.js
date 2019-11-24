85f56bb0566dfcaebe83f4fb42b8a614ad29308c
Stefan Vasic <vasicstefan123@gmail.com>
2019-11-24 01:28:58 +0100

    Updates

--- precache-manifest.9175a1f7f4ed63a74e1a0346938753cd.js
+++ precache-manifest.9175a1f7f4ed63a74e1a0346938753cd.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "846cc36f78d0a06f10c08232ed11f645",
+    "revision": "dd98d2a2ee1e250d6751b73c66632698",
     "url": "/todolist/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/todolist/static/js/2.5d1e4137.chunk.js"
   },
   {
-    "revision": "29d2cfb836e388845ee4",
-    "url": "/todolist/static/js/main.3374b438.chunk.js"
+    "revision": "8a00a702057e9c386637",
+    "url": "/todolist/static/js/main.20d1f351.chunk.js"
   },
   {
     "revision": "1d50dbbb921aaf55723b",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/todolist/precache-manifest.0a6d7158aa9408cf046adb701e12e7f5.js"
+  "/todolist/precache-manifest.9175a1f7f4ed63a74e1a0346938753cd.js"
 );
 
 self.addEventListener('message', (event) => {
